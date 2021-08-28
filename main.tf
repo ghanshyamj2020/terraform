35bfd04dcea54a09287da102bb47919904e851e6
ghanshyamj2020 <83298135+ghanshyamj2020@users.noreply.github.com>
2021-08-28 12:40:20 +0530

    main004

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-medium"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-cloud/centos-7"
     }
   }
 
